70dcd66b92474547548e44d7582457f4bd854cef
Sierra Cheatham <sierracheatham@Sierras-MacBook-Pro.local>
2019-05-05 17:20:37 -0500

    created game logic

---
+++ memory_game/js/main.js
@@ -0,0 +1,9 @@
+console.log('Up and running!');
+
+var cardOne = "queen";
+var cardTwo = "king";
+var CardThree = "queen";
+var cardFour = "king";
+
+console.log("User flipped " + cardOne);
+console.log("User flipped " + cardTwo);
