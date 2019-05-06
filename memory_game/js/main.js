f9632190c7f6cc3947d1c21af56b7e8d711a6c7e
Sierra Cheatham <sierracheatham@Sierras-MacBook-Pro.local>
2019-05-06 15:20:29 -0500

    created game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -1,9 +1,23 @@
-console.log('Up and running!');
+var cards = ["queen", "queen", "king", "king"];
 
-var cardOne = "queen";
-var cardTwo = "king";
-var CardThree = "queen";
-var cardFour = "king";
+var cardsInPlay = [];
+ 
+var cardOne = cards[0];
+var cardTwo = cards[3];
+
+cardsInPlay.push(cardOne);
+cardsInPlay.push(cardTwo);
+
+if (cardsInPlay.length === 2) {
+
+if (cardsInPlay[0] === cardsInPlay[1]) {
+alert("You found a match!");
+} else {
+alert("Sorry, try again.");
+}
+
+}
+console.log("Up and running!");
 
 console.log("User flipped " + cardOne);
 console.log("User flipped " + cardTwo);
